d1e7af7796a075d038bedbb3be096211d314dcb1
xasiax717 <Lewkowicz_Zofia@equ.tele.pw.edu.pl>
2025-11-10 13:47:38 +0100

    very first commit

---
+++ src/main/java/com/booknook/booknook/BookNookApplication.java
@@ -0,0 +1,13 @@
+package com.booknook.booknook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNookApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/booknook/booknook/run/Run.java
@@ -0,0 +1,5 @@
+package com.booknook.booknook.run;
+
+public class Run {
+
+}
