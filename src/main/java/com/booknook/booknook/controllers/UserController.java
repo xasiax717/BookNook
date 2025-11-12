f1e91fe8ba5c855e6c019e67128ad30697fa11c2
xasiax717 <Lewkowicz_Zofia@equ.tele.pw.edu.pl>
2025-11-12 14:09:48 +0100

    login and signup pages

---
+++ src/main/java/com/booknook/booknook/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.booknook.booknook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/signup")
+    public String signup(){
+        return "signup";
+    }
+}
---
+++ src/main/java/com/booknook/booknook/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.booknook.booknook.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+     return httpSecurity
+             .formLogin(httpForm -> {
+                 httpForm
+                     .loginPage("/login").permitAll();
+             })
+
+             .authorizeHttpRequests(registry ->{
+                     registry.requestMatchers("/signup").permitAll();
+                     registry.anyRequest().authenticated();
+             })
+
+             .build();
+    }
+}
